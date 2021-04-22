68f1f2c02eda7f1ebed0afd55c12e76d70b2539b
Olli Janatuinen <olli.janatuinen@gmail.com>
2021-04-22 23:21:13 +0300

    Initial (not fully working) support for k3s

--- 00_settings.sh
+++ 00_settings.sh
@@ -2,7 +2,12 @@ arch=$(arch)
 kernel_version="5.10.31-burmilla"
 debian_version="bullseye"
 docker_version="20.10.6"
+k3s_version="v1.20.6+k3s1"
 output_version="prototype"
+
+# Docker / K3s selection
+container_platform="docker"
+# container_platform="k3s"
 
 
 if [ "$arch" == "x86_64" ]; then
--- __update_all.sh
+++ __update_all.sh
@@ -4,8 +4,8 @@ set -e
 ./10_download_kernel.sh
 ./20_download_rootfs.sh
 ./21_update_rootfs.sh
-./40_download_docker.sh
-./41_update_docker.sh
+./40_download_container_platform.sh
+./41_update_container_platform.sh
 ./50_update.sh
 ./51_test.sh
 
